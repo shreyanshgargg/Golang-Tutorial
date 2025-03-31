16f36fb6e00ae531e6dad99343a0050eb2836cd2
shreyanshgargg <shreyanshgarg834@gmail.com>
2025-03-31 23:19:58 +0530

    added 07mytime

--- 07mytime/main.go
+++ 07mytime/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("welcome to my time")
 
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006 	15:04:05 Monday"))
 }
